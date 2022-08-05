dbbc2d37dc33f279c83090e87802c43c6b5c94f7
jamersonsouza <jamersonsouza@outlook.com>
2022-08-05 01:42:01 -0300

    listagem de categorias do  backend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
+import { HomeComponent } from './components/views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //quando o caminho da rota for ( / ou vazia) ele retorna esse component
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'categorias',
+    component: CategoriaReadComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/template/header/header.component';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
+import { HomeComponent } from './components/views/home/home.component';
+
 
 @NgModule({
   declarations: [
@@ -19,6 +24,8 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     HeaderComponent,
     FooterComponent,
     NavComponent,
+    HomeComponent,
+    CategoriaReadComponent,
 
   ],
   imports: [
@@ -28,6 +35,9 @@ import { MatSidenavModule } from '@angular/material/sidenav';
     MatListModule,
     MatSidenavModule,
     MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    HttpClientModule,
 
   ],
   providers: [],
