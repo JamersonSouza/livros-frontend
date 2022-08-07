e8866e29ba115a74aafdc8dc15af49d31ee6fe67
jamersonsouza <jamersonsouza@outlook.com>
2022-08-07 17:52:21 -0300

    método de criar categoria e msg snackBar implementado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,12 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
 import { HomeComponent } from './components/views/home/home.component';
 
 import {MatButtonModule} from '@angular/material/button';
-
+import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
+import { FormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -28,6 +33,7 @@ import {MatButtonModule} from '@angular/material/button';
     NavComponent,
     HomeComponent,
     CategoriaReadComponent,
+    CategoriaCreateComponent,
 
   ],
   imports: [
@@ -41,6 +47,11 @@ import {MatButtonModule} from '@angular/material/button';
     MatTableModule,
     HttpClientModule,
     MatButtonModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSnackBarModule,
+    BrowserAnimationsModule,
 
   ],
   providers: [],
